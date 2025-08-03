072897ef7936e2dca49bca053da78ec44f9011fa
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-08-03 14:06:43 -0300

    feat: add summaryData parameter to QuestionsSummary widget

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
